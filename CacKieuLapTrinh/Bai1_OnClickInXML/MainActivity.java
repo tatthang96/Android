eb9c980f86a2ee2be8f3ced82fa3eb8954d439c0
tatthang96 <tatthang96@gmail.com>
2017-05-03 16:09:17 +0700

    Add files via upload

---
+++ CacKieuLapTrinh/Bai1_OnClickInXML/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tatth.bai1_onclickinxml;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btn_tong2so(View v){
+        EditText edita = (EditText)findViewById(R.id.editsoa);
+        int a = Integer.parseInt(edita.getText()+"");
+        EditText editb = (EditText)findViewById(R.id.editsob);
+        int b = Integer.parseInt(editb.getText()+"");
+
+        TextView txtkq = (TextView)findViewById(R.id.txtkq);
+        txtkq.setText((a+b)+"");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
